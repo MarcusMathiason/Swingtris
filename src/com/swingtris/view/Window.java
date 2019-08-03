a5ac85bf01d79bd6f9af1bfadc4687f4181d1aa0
MarcusMathiason <marmathi@student.chalmers.se>
2019-08-03 02:17:24 +0200

    Initial commit. Created a Window class

---
+++ src/com/swingtris/view/Window.java
@@ -0,0 +1,11 @@
+package com.swingtris.view;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	public Window(int height, int width) {
+		this.setSize(width, height);
+	}
+}

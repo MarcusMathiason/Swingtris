ef12f941979ece0e72d2abb0da90db3c2460ce10
MarcusMathiason <marmathi@student.chalmers.se>
2019-08-03 02:22:27 +0200

    Created a main Game class creating a window of size 800x600

---
+++ src/model/Game.java
@@ -0,0 +1,15 @@
+package model;
+
+import com.swingtris.view.Window;
+
+public class Game {
+	
+	public Game() {
+		Window window = new Window(800, 600);
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+	}
+
+}
